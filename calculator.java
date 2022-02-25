faa6086922dbfcc50520e8453fe389ef0ec8d35c
GaryAnto <99699734+GaryAnto@users.noreply.github.com>
2022-02-25 07:27:56 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,17 @@
+package project4;
+
+public class calculator {
+	public static void main(String[] args) {
+        
+	        double a=20;
+			double b =10;
+			double c = a*b;
+			System.out.println("c= " + c);
+			
+			float e=30.5f;  
+	        float f=40f;  
+	        float g=e/f;
+	        System.out.println("g= " + g);  
+	        	
+}
+}
